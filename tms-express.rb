614d134a8f547ae7403aaeb1e459b10a4499ddb9
Joseph Bellahcen <joeclb@icloud.com>
2022-12-17 16:32:01 -0600

    Update tms-express.rb

--- tms-express.rb
+++ tms-express.rb
@@ -1,11 +1,12 @@
 class TmsExpress < Formula
   desc "LPC speech encoder for the TMS5220"
   homepage "https://github.com/tornupnegatives/TMS-Express"
-  url "https://github.com/tornupnegatives/TMS-Express/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "c00e54d1d2148b333fc12d2691f17ed457e54ce39e59b8516ddbc2e1f9597e6d"
+  url "https://github.com/tornupnegatives/TMS-Express/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "915a269bf94e5e54448a01d840d83f00a6849865b5d8c3d79806c3d769b0ce4c"
   license "GPL-3.0"
 
   depends_on "cmake" => :build
+  depends_on "pkg-config"
   depends_on "libsamplerate"
   depends_on "libsndfile"
 
